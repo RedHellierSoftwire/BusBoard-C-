1f41d9f5880601cfe40f3289a5be7b89c8433006
RedHellierSoftwire <red.hellier@softwire.com>
2025-09-15 14:06:01 +0100

    Analyzer set up in settings, Rest API called and data deserialized

--- BusBoard/Program.cs
+++ BusBoard/Program.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+
+using BusBoard.Models;
+
 using RestSharp;
-using RestSharp.Authenticators;
 
 namespace BusBoard
 {
   class Program
   {
-    static void Main(string[] args)
-    {
-    }
+        static async Task Main(string[] args)
+        {
+            var options = new RestClientOptions("https://api.tfl.gov.uk/StopPoint");
+
+            var client = new RestClient(options);
+            var request = new RestRequest("{id}/Arrivals")
+                .AddUrlSegment("id", "490008660N")
+                .AddParameter("app_key", "9335b5c020994e4b867efbed7ca4a091");
+
+            var response = await client.GetAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = JsonSerializer.Deserialize<JsonArray>(response.Content!);
+                Console.WriteLine(data?[0]);
+            }
+
+        }
   }
 }
