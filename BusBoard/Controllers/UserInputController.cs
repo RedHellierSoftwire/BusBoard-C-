88fce191e53cd1e26c92c2785fdb1ef79847a1a5
RedHellierSoftwire <red.hellier@softwire.com>
2025-09-17 15:07:54 +0100

    Added UserInputController and input methods

---
+++ BusBoard/Controllers/UserInputController.cs
@@ -0,0 +1,44 @@
+namespace BusBoard.Controllers;
+
+public class UserInputController
+{
+    public string GetStringInputFromUser(string prompt)
+    {
+        Console.Write(prompt);
+        string postcode = Console.ReadLine()!;
+        return postcode;
+    }
+
+    public int GetIntInputFromUser(string prompt)
+    {
+        Console.Write(prompt);
+        try
+        {
+            int integer = int.Parse(Console.ReadLine()!);
+            return integer;
+        }
+        catch (FormatException)
+        {
+            throw new FormatException("Input was not a valid integer");
+        }
+    }
+
+    public string GetPostcodeFromUser()
+    {
+        string postcode = GetStringInputFromUser("Enter Postcode: ");
+
+        postcode = postcode.Replace(" ", "").ToUpper();
+
+        if (string.IsNullOrWhiteSpace(postcode))
+        {
+            throw new ArgumentException("Postcode cannot be empty");
+        }
+
+        if (postcode.Length < 5 || postcode.Length > 7)
+        {
+            throw new ArgumentException("Postcode must be between 5 and 7 characters long");
+        }
+
+        return postcode;
+    }
+}
